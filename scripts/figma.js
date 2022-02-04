659987cdf38c6434d469ade28dcadc8e167bd5ef
takanorip <takanori0170@gmail.com>
2022-02-04 11:57:46 +0900

    Remove request & add node-fetch

--- scripts/figma.js
+++ scripts/figma.js
@@ -1,48 +1,46 @@
-const got = require('got');
-const request = require('request');
 const { resolve } = require('path');
-const fs = require('fs');
+const { createWriteStream } = require('fs');
+const { pipeline } = require('stream');
+const { promisify } = require('util');
 const TOKEN = process.env.FIGMA_TOKEN;
 const FIGMA_FILE_KEY = process.env.FIGMA_FILE_KEY;
 
-const download = (url, path, callback) => {
-  // TODO: requestはdeprecatedなどで別のものに置き換える
-  request.head(url, (err, res, body) => {
-    request(url)
-      .pipe(fs.createWriteStream(path))
-      .on('close', callback)
-  })
-}
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-async function main() {
-  const { body } = await got(`https://api.figma.com/v1/files/${FIGMA_FILE_KEY}/components`, {
+const download = async (url, path, callback) => {
+  const streamPipeline = promisify(pipeline);
+  const response = await fetch(url);
+  await streamPipeline(response.body, createWriteStream(path));
+  callback();
+};
+
+const main = async () => {
+  const response = await fetch(`https://api.figma.com/v1/files/${FIGMA_FILE_KEY}/components`, {
     headers: {
       'X-FIGMA-TOKEN': TOKEN,
     },
-    responseType: 'json',
-  })
-  const results = body.meta.components
-  const ids = results.map(r => r.node_id).join(',')
-  const {
-    body: {
-      images
-    }
-  } = await got(`https://api.figma.com/v1/images/${FIGMA_FILE_KEY}?ids=${ids}&format=svg`, {
+  });
+  const { meta } = await response.json();
+  const results = meta.components;
+  const ids = results.map((r) => r.node_id).join(',');
+
+  const svgResponse = await fetch(`https://api.figma.com/v1/images/${FIGMA_FILE_KEY}?ids=${ids}&format=svg`, {
     headers: {
       'X-FIGMA-TOKEN': TOKEN,
     },
-    responseType: 'json',
-  })
+  });
+  const { images } = await svgResponse.json();
   const nodeIds = Object.keys(images);
+
   for (const nodeId of nodeIds) {
-    const url = images[nodeId]
-    const result = results.find(r => r.node_id === nodeId)
-    const name = result.name
+    const url = images[nodeId];
+    const result = results.find((r) => r.node_id === nodeId);
+    const name = result.name;
     const path = resolve(__dirname, `../assets/${name}.svg`);
     download(url, path, () => {
-      console.log(path,'done!')
-    })
+      console.log(path, 'done!');
+    });
   }
-}
+};
 
 main();

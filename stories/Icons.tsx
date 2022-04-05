92f2145ea6e3131e9744a56ef299d97b1877a2c5
takanorip <takanori0170@gmail.com>
2022-04-05 18:37:30 +0900

    use PascalCase

--- stories/Icons.tsx
+++ stories/Icons.tsx
@@ -2,18 +2,13 @@ import React from 'react';
 import * as IconComponents from '../src/index';
 import './Icons.css'
 
-const pascalToSentence = (text: string): string => {
-  const t = text.replace( /([A-Z])/g, " $1" );
-  return t.charAt(0).toUpperCase() + t.slice(1);
-}
-
 export const Icons = () => {
   return (
     <div className='grid'>
       {Object.keys(IconComponents).map(c => (
         <div className='item' key={c}>
           {IconComponents[c]()}
-          <p className='title'>{pascalToSentence(c)}</p>
+          <p className='title'>{c}</p>
         </div>
       ))}
     </div>

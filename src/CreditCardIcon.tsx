2d738a502b24fb74726c30ef726c322aadc7bb26
hirotaka-yoshii <hirotaka-yoshii@users.noreply.github.com>
2024-08-27 06:19:43 +0000

    feat:🎸 update icons from figma
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/CreditCardIcon.tsx
@@ -0,0 +1,10 @@
+import type { SVGProps } from 'react';
+const SvgCreditCardIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M22 6v12q0 .824-.587 1.413A1.93 1.93 0 0 1 20 20H4q-.824 0-1.412-.587A1.93 1.93 0 0 1 2 18V6q0-.824.587-1.412A1.93 1.93 0 0 1 4 4h16q.824 0 1.413.588Q22 5.175 22 6M4 8h16V6H4zm0 4v6h16v-6z"
+    />
+  </svg>
+);
+export default SvgCreditCardIcon;

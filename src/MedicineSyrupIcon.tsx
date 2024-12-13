24c0a3ce4f8c75208d3b835dce871e74acb36774
chihokotaro <chihokotaro@users.noreply.github.com>
2024-12-13 04:21:14 +0000

    feat:🎸 update icons from figma
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/MedicineSyrupIcon.tsx
@@ -0,0 +1,12 @@
+import type { SVGProps } from 'react';
+const SvgMedicineSyrupIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M5 4a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2v3a3 3 0 0 1 3 3v3h3.5a2 2 0 0 1 1.993 2.166l-.5 6A2 2 0 0 1 20 23h-6a2 2 0 0 1-1.732-1H4a2 2 0 0 1-2-2V10a3 3 0 0 1 3-3zm7 0v3H7V4zM5 9h9a1 1 0 0 1 1 1v1H4v-1a1 1 0 0 1 1-1m-1 4v1.5h5.25a.75.75 0 0 0 0-1.5zm0 3.5V18h5.25a.75.75 0 0 0 0-1.5zM20 21l.5-6h-7l.5 6z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgMedicineSyrupIcon;

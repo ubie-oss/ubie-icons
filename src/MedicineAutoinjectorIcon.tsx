1f63b46ded923a80c2d2661c7f564fb3cea809fe
chihokotaro <chihokotaro@users.noreply.github.com>
2024-12-18 05:19:54 +0000

    feat:🎸 update icons from figma
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/MedicineAutoinjectorIcon.tsx
@@ -0,0 +1,16 @@
+import type { SVGProps } from 'react';
+const SvgMedicineAutoinjectorIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="none" viewBox="0 0 24 24" {...props}>
+    <path
+      fill="currentColor"
+      d="M12.977 10.995a1 1 0 0 1 0 1.414l-2.5 2.5a1 1 0 0 1-1.414-1.414l2.5-2.5a1 1 0 0 1 1.414 0"
+    />
+    <path
+      fill="currentColor"
+      fillRule="evenodd"
+      d="M6.736 22.13a2 2 0 0 1-2.61-.188l-2.122-2.12a2 2 0 0 1-.188-2.612l4.17-5.583q.086-.116.188-.218l9.265-9.266a2 2 0 0 1 2.829 0l3.535 3.536a2 2 0 0 1 0 2.828l-9.265 9.266a2 2 0 0 1-.218.188zm-3.318-3.723 2.122 2.121 2.75-2.053-2.818-2.818zm2.96-3.964 3.126 3.125 1.62-1.21 9.265-9.265-3.535-3.535-9.266 9.265z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgMedicineAutoinjectorIcon;
--- src/MedicineSyrupIcon.tsx
+++ src/MedicineSyrupIcon.tsx
@@ -4,7 +4,7 @@ const SvgMedicineSyrupIcon = (props: SVGProps<SVGSVGElement>) => (
     <path
       fill="currentColor"
       fillRule="evenodd"
-      d="M5 4a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2v3a3 3 0 0 1 3 3v3h3.5a2 2 0 0 1 1.993 2.166l-.5 6A2 2 0 0 1 20 23h-6a2 2 0 0 1-1.732-1H4a2 2 0 0 1-2-2V10a3 3 0 0 1 3-3zm7 0v3H7V4zM5 9h9a1 1 0 0 1 1 1v1H4v-1a1 1 0 0 1 1-1m-1 4v1.5h5.25a.75.75 0 0 0 0-1.5zm0 3.5V18h5.25a.75.75 0 0 0 0-1.5zM20 21l.5-6h-7l.5 6z"
+      d="M5 4a2 2 0 0 1 2-2h5a2 2 0 0 1 2 2v3a3 3 0 0 1 3 3v3h3.5a2 2 0 0 1 1.993 2.166l-.5 6A2 2 0 0 1 20 23h-6a2 2 0 0 1-1.732-1H4a2 2 0 0 1-2-2V10a3 3 0 0 1 3-3zm7 0v3H7V4zM5 9h9a1 1 0 0 1 1 1v1H4v-1a1 1 0 0 1 1-1m-1 4v1.5h3.25a.75.75 0 0 0 0-1.5zm0 3.5V18h3.25a.75.75 0 0 0 0-1.5zM20 21l.5-6h-7l.5 6z"
       clipRule="evenodd"
     />
   </svg>
